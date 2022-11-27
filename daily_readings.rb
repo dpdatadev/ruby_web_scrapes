13aab860651b263c8192cc8bad5ad3fa36dbb7bf
Derek Pauley <dpdatadev@gmail.com>
2022-11-26 21:08:26 -0600

    seriously dude I left one so whatever

--- daily_readings.rb
+++ daily_readings.rb
@@ -12,7 +12,7 @@ log_file = File.open("database.log", File::WRONLY | File::APPEND)
 
 data_log = Logger.new(log_file)
 
-connection = PG.connect(:hostaddr=>"23.239.16.24", :port=>5432, :dbname=>"scrapedata", :user=>"linpostgres", :password=>"")
+connection = PG.connect(:hostaddr=>"23.239.16.24", :port=>5432, :dbname=>"scrapedata", :user=>"linpostgres", :password=>"KHrdU1JRn9H_8EsO")
 
 
 # config variables

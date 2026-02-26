f9a76b08395a1b881d079c2e4d1b96fedbbea1a3
dpdatadev <91690736+dpdatadev@users.noreply.github.com>
2026-02-26 08:26:19 -0600

    Update Postgres connection password to FAKEPASSWORD

--- lib/data/test.rb
+++ lib/data/test.rb
@@ -7,7 +7,7 @@ require 'test/unit'
 class TestPostgres < Test::Unit::TestCase
   def test_postgres_connection_is_valid
     table = Postgres::LinkElement::Table.new('articles', 'orthochristian', '23.239.16.24', 5432, 'scrapedata',
-                                             'linpostgres', 'KHrdU1JRn9H_8EsO')
+                                             'linpostgres', 'FAKEPASSWORD')
     assert_not_nil table
   end
 end
